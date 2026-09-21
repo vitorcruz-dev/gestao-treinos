383e3a2d4085b08bd24d3093c603de82a2ef315e
vitorcruz-dev <vitorcruz@jjteixeira.pt>
2026-09-21 08:52:03 +0100

    login error 4

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://wvlaetlzpkemdpjrztun.supabase.co';
-const supabaseKey = 'sb_publishable_lHsT483fRZJro5X7GDQ4Eg_ojDhZaeE'; // (a sua Publishable key completa)
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2bGFldGx6cGtlbWRwanJ6dHVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3NDE5NjcsImV4cCI6MjEwNTMxNzk2N30.PQG0hm2NWW4lJiJdxUyB6JQCANTXmsbr9qaaupoxtps'; // (a sua Publishable key completa)
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
